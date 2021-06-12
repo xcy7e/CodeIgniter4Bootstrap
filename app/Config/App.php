fd88f5c25e2f53f9b38b45b4fd92a567cac88040
John <johnfiddle7@gmail.com>
2021-06-12 20:35:43 +0200

    add vendor (Bootstrap,jQuery) change baseURL

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://localhost/cibs/public/';
+	public $baseURL = 'http://localhost/CodeIgniter4Bootstrap/public/';
 
 	/**
 	 * --------------------------------------------------------------------------
