ce7342cb00a14adb18e57bb68e992610f30b9513
John <johnfiddle7@gmail.com>
2021-06-12 20:14:56 +0200

    change baseURL

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://localhost/gitdash/public/';
+	public $baseURL = 'http://localhost/cibs/public/';
 
 	/**
 	 * --------------------------------------------------------------------------
