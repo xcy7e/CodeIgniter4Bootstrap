73a69dc0c1952892bc8385db2d173b5f41d423b8
John <johnfiddle7@gmail.com>
2021-06-14 22:47:55 +0200

    resume template fix

--- app/Views/templates/resume/footer.php
+++ app/Views/templates/resume/footer.php
@@ -1,6 +1,9 @@
 
+<!-- jQuery 3.6.0 -->
+<script type="text/javascript" src="<?=base_url('assets/vendor/jquery/jquery-3.6.0/jquery-3.6.0.min.js')?>"></script>
+
 <!-- Bootstrap 5.0.1 JS -->
-<script type="text/javascript" src="<?=base_url('assets/vendor/bootstrap/bootstrap-4.6.0/js/bootstrap.bundle.min.js');?>"></script>
+<script type="text/javascript" src="<?=base_url('assets/vendor/bootstrap/bootstrap-5.0.1/js/bootstrap.bundle.min.js');?>"></script>
 
 <!-- Core theme JS-->
 <script type="text/javascript" src="<?=base_url('assets/templates/resume/js/resume.js')?>"></script>
